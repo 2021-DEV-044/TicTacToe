d5e5493827a969f5e70302f65b7a58793fb8b8d6
2021-DEV-044 <jorenjamar@gmail.com>
2021-04-17 17:46:45 +0200

    show game state in TextView above field
    The game state is shown is shown in the TextView. The game state could be it's x's turn, it is a tie or x is the winner. The text gets update on start, on a play and on reset.

--- app/src/main/java/com/jorenjamar/tictactoe/MainActivity.kt
+++ app/src/main/java/com/jorenjamar/tictactoe/MainActivity.kt
@@ -3,10 +3,7 @@ package com.jorenjamar.tictactoe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.LinearLayout
-import android.widget.Toast
+import android.widget.*
 import androidx.core.view.children
 import androidx.core.view.forEach
 import kotlinx.android.synthetic.main.activity_main.*
@@ -34,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         }
         btnReset.setOnClickListener(listenerReset)
         ttt = TicTacToe(rows, columns, amountOfPlayers, amountToWin)
+        tvGameState.text = "It's ${ttt.selectNextPlayer()}'s turn"
     }
 
     var listenerField = View.OnClickListener { view ->
@@ -43,7 +41,14 @@ class MainActivity : AppCompatActivity() {
         var col = (regexCol.find(view.getTag().toString()))!!.groups[1]!!.value.toInt()
 
         var player =  ttt.selectNextPlayer();
-        if(ttt.makeMove(player, col, row)) (view as Button).text = player.toString()
+        if(ttt.makeMove(player, col, row)){
+            (view as Button).text = player.toString()
+            when(ttt.gameState()){
+                GameState.WINNER -> tvGameState.text = "$player is winner"
+                GameState.TIE -> tvGameState.text = "it is a tie"
+                GameState.CONTINUE -> tvGameState.text = "It's ${ttt.selectNextPlayer()}'s turn"
+            }
+        }
     }
 
     var listenerReset = View.OnClickListener { view ->
@@ -57,5 +62,6 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+        tvGameState.text = "It's ${ttt.selectNextPlayer()}'s turn"
     }
 }
